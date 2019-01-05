52bba20ee782233603efae36ed458e283fda840e
tangxin <tangxin@tangxin-mbp.local>
2019-01-05 22:49:01 +0800

    fix: use NewRandom() replace NewUUID()

--- main.go
+++ main.go
@@ -17,9 +17,9 @@ type Config struct {
 var config Config
 
 func init() {
-	flag.BoolVar(&config.lower, "lower", false, "Generate UUID by lower case (default) ")
+	flag.BoolVar(&config.lower, "lower", true, "Generate UUID by lower case (default) ")
 	flag.BoolVar(&config.upper, "upper", false, "Generate UUID by upper case")
-	flag.IntVar(&config.num, "num", 1, "How many uuid to be generated")
+	flag.IntVar(&config.num, "n", 1, "How many uuid to be generated")
 }
 
 func main() {
@@ -29,14 +29,14 @@ func main() {
 	for i := 0; i < config.num; i++ {
 		id := UUIDGen()
 		func() {
-			if config.lower {
-				fmt.Println(strings.ToLower(id))
-				return
-			}
 			if config.upper {
 				fmt.Println(strings.ToUpper(id))
 				return
 			}
+			// if config.lower {
+			fmt.Println(strings.ToLower(id))
+			return
+			// }
 		}()
 	}
 
@@ -44,10 +44,6 @@ func main() {
 
 // UUIDGen return a uuid
 func UUIDGen() string {
-	id, err := uuid.NewUUID()
-	if err != nil {
-		panic(err)
-	}
-
+	id := uuid.Must(uuid.NewRandom())
 	return id.String()
 }
